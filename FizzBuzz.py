71a76c4cb9be713bf44da1c9554858cd25baf976
alextran1892 <alextran.010892@gmail.com>
2024-03-14 21:47:12 +0700

    FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,19 @@
-Numb = int(input('Nhap hai so bat ky phan tach bang dau phay: '))
-print(Numb.split(','))
-for i in range(1,30):
-    if i%3 == 0 and i%5 == 0:
-        print('FizzBuzz')
-    elif i%5 == 0:
-        print('Buzz')
-    elif i%3 == 0:
-        print('Fizz')
-    else:
-        print(i)
+numb = input('Nhap hai so bat ky phan tach bang dau phay: ')
+numb1 = numb.split(',')
+a = int(numb1[0])
+b = int(numb1[1])
+
+#Xét điều kiện 1
+if a>=b:
+    for i in range(1,30):
+        if i%3 == 0 and i%5 == 0:
+            print('FizzBuzz')
+        elif i%5 == 0:
+            print('Buzz')
+        elif i%3 == 0:
+            print('Fizz')
+        else:
+            print(i)
+else:
+    print("Vui lòng nhập số a lớn hơn hoặc bằng số b")
+
