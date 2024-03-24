5e8d4959e0f08afc77246c47389e1247ceb32e67
alextran1892 <alextran.010892@gmail.com>
2024-03-24 21:14:27 +0700

    continuous clock

--- build_clock_using_class.py
+++ build_clock_using_class.py
@@ -3,13 +3,11 @@ import time
 
 #Xây dựng lớp clock
 class Clock:
-    #Thuộc tính của clock
+    # Thuộc tính của clock
     # def __init__(self, hour, minute, second, r, heart):
     #     self.hour = hour
     #     self.minute = minute
     #     self.second = second
-    #     self.r = r #Bán kính đồng hồ
-    #     self.heart = heart 
 
     # Hàm vẽ mặt của đồng hồ, các vạch chia của đồng hồ
 
@@ -65,6 +63,7 @@ class Clock:
     hour_hand = turtle.Turtle()
     hour_hand.pencolor('yellow')
     hour_hand.pensize(4)
+    hour_hand.speed(100)
     hour_hand.hideturtle()
 
     
@@ -73,6 +72,7 @@ class Clock:
     minute_hand = turtle.Turtle()
     minute_hand.pencolor('black')
     minute_hand.pensize(3)
+    minute_hand.speed(100)
     minute_hand.hideturtle()
 
 
@@ -81,49 +81,66 @@ class Clock:
     second_hand = turtle.Turtle()
     second_hand.pencolor('red')
     second_hand.pensize(2)
+    second_hand.speed(100)
     second_hand.hideturtle()
 
         
     #Cập nhật kim đồng hồ
     def update_current_time(self):
-        
-        for i in range 1:
+
+        while True:
             current_time = time.localtime()
             hour = current_time.tm_hour
             minute = current_time.tm_min
             second = current_time.tm_sec
 
+
             #Tính góc cho các kim đồng hồ
             angle_hour_hand = (hour % 12) * 30 + (minute/60) * 30
             angle_minute_hand = minute *6 + (second / 60) * 6
             angle_second_hand = second * 6
-            self.second_hand.setheading(90 - angle_second_hand)
-            #Đặt vị trí cho kim đồng hồ
-            self.second_hand.forward(170)
-            self.second_hand.backward(170)
-            time.sleep(0.3)
-            self.second_hand.clear()
-            
-            self.minute_hand.setheading(90 - angle_minute_hand)
-            self.minute_hand.forward(150)
-            self.minute_hand.backward(150)
-            # self.minute_hand.clear()
 
             self.hour_hand.setheading(90 - angle_hour_hand)
             self.hour_hand.forward(100)
             self.hour_hand.backward(100)
-            # time.sleep(3600)
-            # self.hour_hand.clear()
+          
+            for i in range (60):
+                current_time = time.localtime()
+                hour = current_time.tm_hour
+                minute = current_time.tm_min
+                second = current_time.tm_sec
 
 
+                #Tính góc cho các kim đồng hồ
+                angle_hour_hand = (hour % 12) * 30 + (minute/60) * 30
+                angle_minute_hand = minute *6 + (second / 60) * 6
+                angle_second_hand = second * 6               
+                self.minute_hand.setheading(90 - angle_minute_hand)
+                self.minute_hand.forward(150)
+                self.minute_hand.backward(150)
+                # self.minute_hand.clear()
+                #Đặt vị trí cho kim đồng hồ
+                for k in range (60):
+                    current_time = time.localtime()
+                    hour = current_time.tm_hour
+                    minute = current_time.tm_min
+                    second = current_time.tm_sec
 
+
+                    #Tính góc cho các kim đồng hồ
+                    angle_hour_hand = (hour % 12) * 30 + (minute/60) * 30
+                    angle_minute_hand = minute *6 + (second / 60) * 6
+                    angle_second_hand = second * 6                   
+                    self.second_hand.setheading(90 - angle_second_hand)
+                    self.second_hand.forward(170)
+                    self.second_hand.backward(170)
+                    time.sleep(0.9)
+                    self.second_hand.clear()
+                self.minute_hand.clear()
+            self.hour_hand.clear()
 
             #Cập nhật màn hình
             self.screen.update()
-
-
-
-        
 
 c = Clock()
 c.update_current_time()
